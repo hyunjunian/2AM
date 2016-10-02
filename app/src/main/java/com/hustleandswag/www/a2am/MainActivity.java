e758d103e0f70fe2e1bad5388be36e3cd4704256
Hyunjun Jeon <hyunjunjeon@Hyunjunui-MacBook-Pro.local>
2016-10-02 18:12:00 +0900

    recycleview test

--- app/src/main/java/com/hustleandswag/www/a2am/MainActivity.java
+++ app/src/main/java/com/hustleandswag/www/a2am/MainActivity.java
@@ -1,22 +1,71 @@
 package com.hustleandswag.www.a2am;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.digits.sdk.android.AuthCallback;
+import com.digits.sdk.android.AuthConfig;
+import com.digits.sdk.android.Digits;
+import com.digits.sdk.android.DigitsAuthButton;
+import com.digits.sdk.android.DigitsException;
+import com.digits.sdk.android.DigitsSession;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import io.fabric.sdk.android.Fabric;
 
 public class MainActivity extends AppCompatActivity {
+    // Note: Your consumer key and secret should be obfuscated in your source code before shipping.
+    private static final String TWITTER_KEY = "ctO9HO0w1F4hcaL8pxvfI15KT";
+    private static final String TWITTER_SECRET = "1CMWCeiPLCXZKHqSQHfUYR0geUxwCVxHPqkWjU3n1JFvj31mFw";
+    private AuthCallback authCallback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //트위터 전화번호 인증 api
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        /*인증 창 테마 바꾸기
+        Digits.Builder digitsBuilder = new Digits.Builder().withTheme(R.style.CustomDigitsTheme);
+        Fabric.with(this, new TwitterCore(authConfig), digitsBuilder.build());*/
+        Fabric.with(this, new TwitterCore(authConfig), new Digits.Builder().build());
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        authCallback = new AuthCallback() {
+            @Override
+            public void success(DigitsSession session, String phoneNumber) {
+                // TODO: associate the session userID with your user model
+                Toast.makeText(getApplicationContext(), "Authentication successful for "
+                        + phoneNumber, Toast.LENGTH_LONG).show();
+            }
+
+            @Override
+            public void failure(DigitsException exception) {
+                Log.d("Digits", "Sign in with Digits failure", exception);
+            }
+        };
+
+        /*이메일까지 입력하게 하기
+        AuthConfig.Builder authConfigBuilder = new AuthConfig.Builder()
+                .withAuthCallBack(authCallback)
+                .withEmailCollection();
+
+        Digits.authenticate(authConfigBuilder.build());*/
+        //인증 버튼 생성
+        DigitsAuthButton digitsButton = (DigitsAuthButton) findViewById(R.id.auth_button);
+        digitsButton.setCallback(this.getAuthCallback());
+        //인증버튼 화면 스타일 설정
+        digitsButton.setAuthTheme(android.R.style.Theme_Material);
+
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -27,6 +76,10 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+
+    public AuthCallback getAuthCallback(){
+        return authCallback;
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -44,9 +97,15 @@ public class MainActivity extends AppCompatActivity {
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Digits.uploadContacts();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void recycler(View view){
+        Intent intent = new Intent(this, RecyclerActivity.class);
+        startActivity(intent);
     }
 }
---
+++ app/src/main/java/com/hustleandswag/www/a2am/RecyclerActivity.java
@@ -0,0 +1,33 @@
+package com.hustleandswag.www.a2am;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class RecyclerActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recycler);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        String[] myDataset = new String[]{"ㅋㅋㅋ","전현준"};
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+}
